45d5e6ba17393ec8784680593134b901d96146c1
Tehepicdan <tehepicdan@gmail.com>
2018-12-13 16:49:55 +0000

    update Today's log:)

--- shell_scripting.sh
+++ shell_scripting.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#update date daily at 6:00 AM
-#Author: Solomon Amos
 
 DATE=`date "+%d-%m-%Y %a %p %l:%M %Z"`
 Weather=$(curl wttr.in/?0?q?T | awk '/°(C|F)/ {printf $(NF-1) $(NF) " ("a")"} /,/ {a=$0}')
 time=$(date +%d-%m-%Y).png
 
-echo -e "* $DATE   : $Weather" | tee -a ~/projects/shell_scripting/README.md
+echo -e "* $DATE   : $Weather" | tee -a ~/Documents/prog/shell/shell_scripting/README.md
 
-date +%d-%m-%Y >> ~/projects/shell_scripting/weather.md
+date +%d-%m-%Y >> ~/Documents/prog/shell/shell_scriptingweather.md
 
-curl -L 'wttr.in/York=.png' -o ~/projects/shell_scripting/img/$time
+curl -L 'wttr.in/York=.png' -o ~/Documents/prog/shell/shell_scripting/img/$time
  
-echo -e "\n![](img/$time)\n" >> ~/projects/shell_scripting/weather.md
+echo -e "\n![](img/$time)\n" >> ~/Documents/prog/shell/shell_scripting/weather.md
 
-cd ~/projects/shell_scripting
+cd ~/Documents/prog/shell/shell_scripting
 git init
 git add .
 git commit -m "update Today's log:)"
-# git remote add origin https://github.com/udohsolomon/shell_scripting.git
+# git remote add origin https://github.com/tehepicdan/shell_scripting.git
 git push -f origin master
+
+
